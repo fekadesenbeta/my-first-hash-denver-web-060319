a34e5615531026c0299871249aa03f11c6b5436a
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-17 21:14:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,8 +38,6 @@ def adding
                   "oil paintings" => 3
                   
                  }
-                  shipping_manifest["muskets"]= 2, 
-                  shipping_manifest["gun powder"] = 4
-                  
-
 end
+shipping_manifest["muskets"]= 2, 
+shipping_manifest["gun powder"] = 4
